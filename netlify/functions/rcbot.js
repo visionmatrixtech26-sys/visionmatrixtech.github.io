9eab629fbda1322ff14839862a0ca0dbb0ec1552
Honey Badger <badger.h001@gmail.com>
2026-05-06 21:33:32 +0530

    Update rcbot.js

--- netlify/functions/rcbot.js
+++ netlify/functions/rcbot.js
@@ -81,6 +81,7 @@ exports.handler = async function (event) {
   const headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'POST, OPTIONS',
     'Content-Type': 'application/json',
   };
 
